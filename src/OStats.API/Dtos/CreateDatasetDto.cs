6d86f57f423859cfc25a60eae0743c7e4909841a
Guilherme <g.lisboa.oliveira@outlook.com>
2024-02-20 18:13:37 +0000

    fix: fix dtos data annotations

--- src/OStats.API/Dtos/CreateDatasetDto.cs
+++ src/OStats.API/Dtos/CreateDatasetDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace OStats.API.Dtos;
 
 public record CreateDatasetDto
 {
+    [Required]
     public required string Title { get; init; }
+
+    [Required]
     public required string Source { get; init; }
     public string? Description { get; init; }
 }
--- src/OStats.API/Dtos/UpdateProjectDto.cs
+++ src/OStats.API/Dtos/UpdateProjectDto.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace OStats.API.Dtos;
 
 public record UpdateProjectDto : CreateProjectDto
 {
+    [Required]
     public required DateTime LastUpdatedAt { get; init; }
 }
