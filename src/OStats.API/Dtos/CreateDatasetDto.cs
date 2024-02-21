78c7870e1e44a7a757325fe1063860e59dd95ead
Guilherme <g.lisboa.oliveira@outlook.com>
2024-02-21 18:51:19 +0000

    fix: fix data annotations on create dtos

--- src/OStats.API/Dtos/CreateDatasetDto.cs
+++ src/OStats.API/Dtos/CreateDatasetDto.cs
@@ -4,10 +4,14 @@ namespace OStats.API.Dtos;
 
 public record CreateDatasetDto
 {
-    [Required]
+    [Required(AllowEmptyStrings = false)]
+    [Length(1, 256)]
     public required string Title { get; init; }
 
-    [Required]
+    [Required(AllowEmptyStrings = false)]
+    [Length(1, 256)]
     public required string Source { get; init; }
+
+    [MaxLength(4096)]
     public string? Description { get; init; }
 }
--- src/OStats.API/Dtos/CreateUserDto.cs
+++ src/OStats.API/Dtos/CreateUserDto.cs
@@ -4,10 +4,12 @@ namespace OStats.API.Dtos;
 
 public struct CreateUserDto
 {
-    [Required]
+    [Required(AllowEmptyStrings = false)]
+    [Length(1, 128)]
     public required string Name { get; init; }
 
-    [Required]
+    [Required(AllowEmptyStrings = false)]
     [EmailAddress]
+    [MaxLength(128)]
     public required string Email { get; init; }
 }
