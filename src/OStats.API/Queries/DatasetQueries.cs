12023e2f46bc0eb9613b9a69a784c4055219c7ce
Guilherme <g.lisboa.oliveira@outlook.com>
2024-02-23 13:53:27 +0000

    fix: revert ef compiled queries

--- src/OStats.API/Queries/DatasetQueries.cs
+++ src/OStats.API/Queries/DatasetQueries.cs
@@ -8,38 +8,38 @@ namespace OStats.API.Queries;
 
 public static class DatasetQueries
 {
-    public static readonly Func<Context, string, Guid, Task<Dataset?>> GetDatasetByIdAsync = EF.CompileAsyncQuery(
-        (Context context, string userAuthId, Guid datasetId) =>
-            context.DatasetsUsersAccessLevels
-                .Join(
-                    context.Datasets,
-                    userAccess => userAccess.DatasetId,
-                    dataset => dataset.Id,
-                    (userAccess, dataset) => new { dataset, userAccess.UserId })
-                .Join(
-                    context.Users,
-                    datasetAndUserId => datasetAndUserId.UserId,
-                    user => user.Id,
-                    (datasetAndUserId, user) => new { datasetAndUserId, user })
-                .Where(joined => joined.datasetAndUserId.dataset.Id == datasetId &&
-                                 joined.user.AuthIdentity == userAuthId)
-                .Select(joined => joined.datasetAndUserId.dataset)
-                .AsNoTracking()
-                .SingleOrDefault()
-    );
+    public static Task<Dataset?> GetDatasetByIdAsync(Context context, string userAuthId, Guid datasetId)
+    {
+        return context.DatasetsUsersAccessLevels
+            .Join(
+                context.Datasets,
+                userAccess => userAccess.DatasetId,
+                dataset => dataset.Id,
+                (userAccess, dataset) => new { dataset, userAccess.UserId })
+            .Join(
+                context.Users,
+                datasetAndUserId => datasetAndUserId.UserId,
+                user => user.Id,
+                (datasetAndUserId, user) => new { datasetAndUserId, user })
+            .Where(joined => joined.datasetAndUserId.dataset.Id == datasetId &&
+                                joined.user.AuthIdentity == userAuthId)
+            .Select(joined => joined.datasetAndUserId.dataset)
+            .AsNoTracking()
+            .SingleOrDefaultAsync();
+    }
 
-    public static readonly Func<Context, Guid, Task<List<User>>> GetDatasetUsersAsync = EF.CompileAsyncQuery(
-        (Context context, Guid datasetId) =>
-            context.DatasetsUsersAccessLevels
-                .Where(datasetAccess => datasetAccess.DatasetId == datasetId)
-                .Join(
-                    context.Users,
-                    datasetAndUserId => datasetAndUserId.UserId,
-                    user => user.Id,
-                    (datasetAndUserId, user) => user)
-                .AsNoTracking()
-                .ToList()
-    );
+    public static Task<List<User>> GetDatasetUsersAsync(Context context, Guid datasetId)
+    {
+        return context.DatasetsUsersAccessLevels
+            .Where(datasetAccess => datasetAccess.DatasetId == datasetId)
+            .Join(
+                context.Users,
+                datasetAndUserId => datasetAndUserId.UserId,
+                user => user.Id,
+                (datasetAndUserId, user) => user)
+            .AsNoTracking()
+            .ToListAsync();
+    }
 
     public static Task<List<DatasetProjectLinkDto>> GetDatasetLinkedProjectsAsync(Context context, Guid datasetId)
     {
