69db70d1e00ad4f4df9c0c1a064715be18904463
Guilherme <g.lisboa.oliveira@outlook.com>
2024-02-23 13:53:09 +0000

    fix: update json constructor for dtos

--- src/OStats.API/Dtos/BaseProjectDto.cs
+++ src/OStats.API/Dtos/BaseProjectDto.cs
@@ -1,15 +1,23 @@
+using System.Text.Json.Serialization;
 using OStats.Domain.Aggregates.ProjectAggregate;
 
 namespace OStats.API.Dtos;
 
 public record BaseProjectDto : BaseEntityDto
 {
-    public string Title { get; }
-    public string? Description { get; }
+    public string Title { get; init;}
+    public string? Description { get; init; }
 
     public BaseProjectDto(Project project) : base(project)
     {
         Title = project.Title;
         Description = project.Description;
+    }
+
+    [JsonConstructor]
+    public BaseProjectDto(Guid id, DateTime createdAt, DateTime lastUpdatedAt, string title, string? description) : base(id, createdAt, lastUpdatedAt)
+    {
+        Title = title;
+        Description = description;
     }
 }
