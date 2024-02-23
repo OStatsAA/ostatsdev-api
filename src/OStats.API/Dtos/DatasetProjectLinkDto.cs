67e6ad968b534b4b1410ca036de781f6e817a2b3
Guilherme <g.lisboa.oliveira@outlook.com>
2024-02-23 23:45:56 +0000

    fix: remove unacessary properties of dataprojectlinkdto

--- src/OStats.API/Dtos/DatasetProjectLinkDto.cs
+++ src/OStats.API/Dtos/DatasetProjectLinkDto.cs
@@ -5,14 +5,10 @@ namespace OStats.API.Dtos;
 
 public record DatasetProjectLinkDto : BaseEntityDto
 {
-    public Guid DatasetId { get; }
-    public Guid ProjectId { get; }
-    public BaseDatasetDto Dataset { get; }
-    public BaseProjectDto Project { get; }
+    public BaseDatasetDto Dataset { get; init; }
+    public BaseProjectDto Project { get; init; }
     public DatasetProjectLinkDto(DatasetProjectLink link, Dataset dataset, Project project) : base(link)
     {
-        DatasetId = link.DatasetId;
-        ProjectId = link.ProjectId;
         Project = new BaseProjectDto(project);
         Dataset = new BaseDatasetDto(dataset);
     }
