653e85f1e648197036d1a7f3cf3be386780fa0e3
Guilherme <g.lisboa.oliveira@outlook.com>
2024-02-24 19:44:39 +0000

    chore: config coverage exclusions

--- scripts/get_test_coverage_report.sh
+++ scripts/get_test_coverage_report.sh
@@ -9,8 +9,13 @@ if [ ! -d "$RESULTS_DIR" ]; then
 fi
 
 # Run tests
+# Exclude DataServiceGrpc (generated code by protoc) and OStats.Infrastructure.Migrations.ContextModelSnapshot (EF Core migration snapshot file)
+# If more settings are needed, move them to a .runsettings file and reference it with --settings as per https://github.com/coverlet-coverage/coverlet/blob/master/Documentation/VSTestIntegration.md
 echo "Running tests..."
-dotnet test src/ --collect:"XPlat Code Coverage" --results-directory:$RESULTS_DIR | tee $TEST_OUTPUT_FILE
+dotnet test src/ \
+  --collect:"XPlat Code Coverage;Exclude=[*]DataServiceGrpc*,[*]OStats.Infrastructure.Migrations.ContextModelSnapshot*" \
+  --results-directory:$RESULTS_DIR \
+  | tee $TEST_OUTPUT_FILE
 
 # Get UUID from last line of test output
 run_uuid=$(tail -n 1 $TEST_OUTPUT_FILE | awk -F '.coverage_report/' '{print substr($2, 1, 36)}')
