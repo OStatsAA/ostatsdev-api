d460e973effe823d2446182b5330bb076ec5b3ba
Guilherme <g.lisboa.oliveira@outlook.com>
2024-02-25 01:14:05 +0000

    test: add get dataset users query test

---
+++ src/OStats.Tests/IntegrationTests/Queries/GetDatasetUsersIntegrationTest.cs
@@ -0,0 +1,34 @@
+using FluentAssertions.Execution;
+using Microsoft.EntityFrameworkCore;
+using OStats.API.Queries;
+using OStats.Domain.Aggregates.DatasetAggregate;
+using OStats.Domain.Aggregates.UserAggregate;
+
+namespace OStats.Tests.IntegrationTests.Queries;
+
+public class GetDatasetUsersIntegrationTest : BaseIntegrationTest
+{
+    public GetDatasetUsersIntegrationTest(IntegrationTestWebAppFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task Shoud_Get_Dataset_Users()
+    {
+        var owner = await context.Users.FirstAsync();
+        var dataset = new Dataset(owner.Id, "Title", "Source");
+        await context.AddAsync(dataset);
+        var editor = new User("Editor", "editor@email.com", "editor_authid");
+        await context.AddAsync(editor);
+        dataset.GrantUserAccess(editor.Id, DatasetAccessLevel.Editor, owner.Id);
+        await context.SaveChangesAsync();
+
+        var datasetUsers = await DatasetQueries.GetDatasetUsersAsync(context, dataset.Id);
+
+        using (new AssertionScope())
+        {
+            datasetUsers.Should().NotBeNullOrEmpty();
+            datasetUsers.Should().HaveCount(2);
+        }
+    }
+}
