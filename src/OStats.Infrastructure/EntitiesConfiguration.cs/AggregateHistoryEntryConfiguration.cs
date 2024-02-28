d542a640c287f9589d5cfb8a61495fd428ff87be
Guilherme <g.lisboa.oliveira@outlook.com>
2024-02-28 17:58:22 +0000

    feat: add aggregates history entries

---
+++ src/OStats.Infrastructure/EntitiesConfiguration.cs/AggregateHistoryEntryConfiguration.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OStats.Domain.Common;
+
+namespace OStats.Infrastructure.EntitiesConfiguration;
+
+public class AggregateHistoryEntryConfiguration : IEntityTypeConfiguration<AggregateHistoryEntry>
+{
+    public void Configure(EntityTypeBuilder<AggregateHistoryEntry> builder)
+    {
+        builder.HasKey(entry => entry.Id);
+        builder.HasIndex(entry => entry.AggregateId);
+        builder.HasIndex(entry => entry.UserId);
+    }
+}
---
+++ src/OStats.Infrastructure/Migrations/20240228175550_AddAggregatesHistoryEntries.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OStats.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAggregatesHistoryEntries : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AggregatesHistoryEntries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AggregateId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AggregateType = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    EventType = table.Column<string>(type: "text", nullable: false),
+                    EventData = table.Column<string>(type: "text", nullable: false),
+                    EventDescription = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AggregatesHistoryEntries", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AggregatesHistoryEntries_AggregateId",
+                table: "AggregatesHistoryEntries",
+                column: "AggregateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AggregatesHistoryEntries_UserId",
+                table: "AggregatesHistoryEntries",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AggregatesHistoryEntries");
+        }
+    }
+}
