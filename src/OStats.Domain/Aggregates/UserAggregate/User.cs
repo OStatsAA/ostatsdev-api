d63abc4cb3424ede16d3b9d53cdd5d9ff7b71f6b
Guilherme <g.lisboa.oliveira@outlook.com>
2024-02-28 11:29:22 +0000

    feat: implement interface in project and user aggregates

--- src/OStats.Domain/Aggregates/UserAggregate/User.cs
+++ src/OStats.Domain/Aggregates/UserAggregate/User.cs
@@ -5,15 +5,15 @@ namespace OStats.Domain.Aggregates.UserAggregate;
 
 public class User : Entity, IAggregateRoot
 {
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string AuthIdentity { get; private set; }
+    private readonly List<IDomainEvent> _domainEvents = new List<IDomainEvent>();
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
     public User(string name, string email, string authIdentity)
     {
         Name = name;
         Email = email;
         AuthIdentity = authIdentity;
     }
-
-    public string Name { get; private set; }
-    public string Email { get; private set; }
-    public string AuthIdentity { get; private set; }
-
 }
