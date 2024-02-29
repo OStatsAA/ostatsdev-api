d652da83ad6bc7dbb775c1e6ee8e3d0904b9a674
Guilherme <g.lisboa.oliveira@outlook.com>
2024-02-29 19:51:35 +0000

    feat: add AddAggregateHistoryEntryCommand

---
+++ src/OStats.API/Commands/AddAggregateHistoryEntryCommand.cs
@@ -0,0 +1,12 @@
+namespace OStats.API.Commands;
+
+public record AddAggregateHistoryEntryCommand
+{
+    public required Guid AggregateId { get; init; }
+    public required string AggregateType { get; init; }
+    public required Guid UserId { get; init; }
+    public required string EventType { get; init; }
+    public required string EventData { get; init; }
+    public required string EventDescription { get; init; }
+    public required DateTime TimeStamp { get; init; }
+}
---
+++ src/OStats.API/Commands/AddAggregateHistoryEntryCommandHandler.cs
@@ -0,0 +1,31 @@
+using MassTransit;
+using OStats.Domain.Common;
+using OStats.Infrastructure;
+
+namespace OStats.API.Commands;
+
+public class AddAggregateHistoryEntryCommandHandler : IConsumer<AddAggregateHistoryEntryCommand>
+{
+    private readonly Context _context;
+
+    public AddAggregateHistoryEntryCommandHandler(Context context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task Consume(ConsumeContext<AddAggregateHistoryEntryCommand> context)
+    {
+        var request = context.Message;
+        _context.AggregatesHistoryEntries.Add(new AggregateHistoryEntry(
+            request.AggregateId,
+            request.AggregateType,
+            request.UserId,
+            request.EventType,
+            request.EventData,
+            request.EventDescription,
+            request.TimeStamp
+        ));
+        await _context.SaveChangesAsync();
+    }
+
+}
