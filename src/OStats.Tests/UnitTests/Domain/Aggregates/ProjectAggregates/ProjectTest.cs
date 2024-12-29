1fe07722870d3d8a05cb7bcb8c6c938554574118
Guilherme <g.lisboa.oliveira@outlook.com>
2024-12-29 19:05:25 +0000

    test: add title and description tests

--- src/OStats.Tests/UnitTests/Domain/Aggregates/ProjectAggregates/ProjectTest.cs
+++ src/OStats.Tests/UnitTests/Domain/Aggregates/ProjectAggregates/ProjectTest.cs
@@ -1,4 +1,5 @@
 using OStats.Domain.Aggregates.ProjectAggregate;
+using OStats.Domain.Aggregates.ProjectAggregate.Events;
 
 namespace OStats.Tests.UnitTests.Domain.Aggregates.ProjectAggregate;
 
@@ -69,5 +70,59 @@ public class ProjectTest
         project.GetUserRole(randomGuid)
                .Should()
                .BeNull();
+    }
+
+    [Fact]
+    public void Should_Be_Able_To_Update_Project_Title()
+    {
+        var ownerId = Guid.NewGuid();
+        var project = new Project(ownerId, "Test Title", "Test Description");
+        var newTitle = "New Title";
+
+        project.SetTitle(newTitle, project.GetUserRole(ownerId)!);
+
+        project.Title.Should().Be(newTitle);
+        project.DomainEvents.Should().ContainSingle(domainEvent => domainEvent is TitleUpdate);
+    }
+
+    [Fact]
+    public void Should_Fail_To_Update_Project_Title_If_Requestor_Is_Not_An_Editor()
+    {
+        var ownerId = Guid.NewGuid();
+        var project = new Project(ownerId, "Test Title", "Test Description");
+        var newTitle = "New Title";
+
+        var result = project.SetTitle(newTitle, new Role(ownerId, ownerId, AccessLevel.ReadOnly));
+
+        result.Succeeded.Should().BeFalse();
+        project.Title.Should().NotBe(newTitle);
+        project.DomainEvents.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Should_Be_Able_To_Update_Project_Description()
+    {
+        var ownerId = Guid.NewGuid();
+        var project = new Project(ownerId, "Test Title", "Test Description");
+        var newDescription = "New Description";
+
+        project.SetDescription(newDescription, project.GetUserRole(ownerId)!);
+
+        project.Description.Should().Be(newDescription);
+        project.DomainEvents.Should().ContainSingle(domainEvent => domainEvent is DescriptionUpdate);
+    }
+
+    [Fact]
+    public void Should_Fail_To_Update_Project_Description_If_Requestor_Is_Not_An_Editor()
+    {
+        var ownerId = Guid.NewGuid();
+        var project = new Project(ownerId, "Test Title", "Test Description");
+        var newDescription = "New Description";
+
+        var result = project.SetDescription(newDescription, new Role(ownerId, ownerId, AccessLevel.ReadOnly));
+
+        result.Succeeded.Should().BeFalse();
+        project.Description.Should().NotBe(newDescription);
+        project.DomainEvents.Should().BeEmpty();
     }
 }
