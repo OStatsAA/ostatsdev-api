4e231f60f8cb853d8afe2d26b87e0b60639bd993
Guilherme <g.lisboa.oliveira@outlook.com>
2025-01-24 20:59:46 +0000

    refactor: make entity guid init one-liner

--- src/OStats.Domain/Common/Entity.cs
+++ src/OStats.Domain/Common/Entity.cs
@@ -7,18 +7,7 @@ namespace OStats.Domain.Common;
 public abstract class Entity
 {
     private Guid _id;
-    public virtual Guid Id
-    {
-        get
-        {
-            return _id;
-        }
-        protected set
-        {
-            _id = value;
-        }
-    }
-
+    public virtual Guid Id { get => _id; protected set => _id = value; }
     public DateTime CreatedAt { get; internal set; }
     public DateTime LastUpdatedAt { get; internal set; }
     public bool IsDeleted { get; internal set; }
